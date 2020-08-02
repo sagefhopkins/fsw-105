41612a4d26dce155f486132caeee69710d1e1d0a
sagefhopkins <sagefhopkins>
2020-08-01 20:42:11 -0600

    Inital commit

---
+++ week1/index.js
@@ -0,0 +1,17 @@
+var shopper = {
+    name: "Joe",
+    age: 21,
+    isAlive: true,
+    purchases: [55,24,33,15,12],
+    purchaseDates: ["11/15/2020", "5/12/2019", "3/3/2019", "12/18/2018", "7/31/2018"],
+    cart: ["Lettuce", "Spinach", "Sunflower Seeds", "Redbull"],
+    shopperData: function(){
+        console.log(this.name);
+        console.log(this.age);
+        console.log(this.isAlive);
+        console.log(purchases);
+        console.log(purchaseDates);
+        console.log(cart);
+    }
+}
+
